9d99bb5f7b6b73180d7edf0fb7348e738225cbe6
bebbiwebbi@gmail.com <bebbiwebbi@gmail.com@fcdf12c8-19f8-012b-3f5a-fcc496d25d5c>
2012-04-03 03:35:21 +0000

    added SiteMapLabel so the text can be changed

--- trunk/September22/September22.Web/Master/Site_2Col.master.designer.cs
+++ trunk/September22/September22.Web/Master/Site_2Col.master.designer.cs
@@ -22,6 +22,15 @@ namespace September22.Master {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// SiteMapLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label SiteMapLabel;
+        
+        /// <summary>
         /// siteMap control.
         /// </summary>
         /// <remarks>
