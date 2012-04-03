bce3a999133bd7265c7708f7777d4106b5e0288a
bebbiwebbi@gmail.com <bebbiwebbi@gmail.com@fcdf12c8-19f8-012b-3f5a-fcc496d25d5c>
2012-04-03 02:27:18 +0000

    added route collection

--- September22/September22.Web/Scripts/PrimaryScript.js
+++ September22/September22.Web/Scripts/PrimaryScript.js
@@ -17,34 +17,55 @@ function setCurrentLavaLamp() {
     });
 
     var url = window.location.href;
-    var lampPage = url.substring(url.indexOf("Site/") + 5);
-
-    switch (lampPage) {
-        case "Home.aspx":
-            $("#liHome").addClass('current');
-            break;
-        case "OurStory.aspx":
-            $("#liOurStory").addClass('current');
-            break;
-        case "OurPhotos.aspx":
-            $("#liOurPhotos").addClass('current');
-            break;
-        case "CeremonyAndReception.aspx":
-            $("#liCeremonyAndReception").addClass('current');
-            break;
-        case "WeddingParty.aspx":
-            $("#liWeddingParty").addClass('current');
-            break;
-        case "Map.aspx":
-            $("#liMap").addClass('current');
-            break;
-        case "Rsvp.aspx":
-            $("#liRsvp").addClass('current');
-            break;
-        default:
-            $("#liHome").addClass('current');
-            break;
+    if (url.indexOf("/Home/") > 0) {
+        $("#liHome").addClass('current');
     }
+    else if (url.indexOf("/Story/") > 0) {
+        $("#liOurStory").addClass('current');
+    }
+    else if (url.indexOf("/Photo/") > 0) {
+        $("#liOurPhotos").addClass('current');
+    }
+    else if (url.indexOf("/Party/") > 0) {
+        $("#liWeddingParty").addClass('current');
+    }
+    else if (url.indexOf("/Map/") > 0) {
+        $("#liMap").addClass('current');
+    }
+    else if (url.indexOf("/RSVP/") > 0) {
+        $("#liRsvp").addClass('current');
+    }
+    else {
+        $("#liHome").addClass('current');
+    }
+
+//    var lampPage = url.substring(url.indexOf("/Site") + 5);
+//    switch (lampPage) {
+//        case "Home.aspx":
+//            $("#liHome").addClass('current');
+//            break;
+//        case "OurStory.aspx":
+//            $("#liOurStory").addClass('current');
+//            break;
+//        case "OurPhotos.aspx":
+//            $("#liOurPhotos").addClass('current');
+//            break;
+//        case "CeremonyAndReception.aspx":
+//            $("#liCeremonyAndReception").addClass('current');
+//            break;
+//        case "WeddingParty.aspx":
+//            $("#liWeddingParty").addClass('current');
+//            break;
+//        case "Map.aspx":
+//            $("#liMap").addClass('current');
+//            break;
+//        case "Rsvp.aspx":
+//            $("#liRsvp").addClass('current');
+//            break;
+//        default:
+//            $("#liHome").addClass('current');
+//            break;
+//    }
 };
 
 $(document).ready(initMenu);
@@ -63,11 +84,11 @@ $(document).ready(function () {
     $('#parallax_bg').plaxify({ "xRange": 0, "yRange": 20, "invert": true })
     $('#parallax_error_text').plaxify({ "xRange": 20, "yRange": 10 })
     $('#parallax_octocat').plaxify({ "xRange": 10, "yRange": 10 })
-    $('#parallax_octocatshadow').plaxify({ "xRange": 10, "yRange": 10 })
-    $('#parallax_speeder').plaxify({ "xRange": 30, "yRange": 30 })
-    $('#parallax_speedershadow').plaxify({ "xRange": 30, "yRange": 30 })
-    $('#parallax_building_1').plaxify({ "xRange": 60, "yRange": 30, "invert": true })
-    $('#parallax_building_2').plaxify({ "xRange": 80, "yRange": 40, "invert": true })
+    $('#parallax_octocatshadow').plaxify({ "xRange": 10, "yRange": 20 })
+    $('#parallax_speeder').plaxify({ "xRange": 20, "yRange": 20 })
+    $('#parallax_speedershadow').plaxify({ "xRange": 20, "yRange": 20 })
+    $('#parallax_building_1').plaxify({ "xRange": 30, "yRange": 10, "invert": true })
+    $('#parallax_building_2').plaxify({ "xRange": 40, "yRange": 20, "invert": true })
 
     $.plax.enable({ "activityTarget": $('#parallax_shell') })
 })
