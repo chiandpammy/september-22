e12dd440f0fbfb9d19992c51b5242e6ad8f890d3
bebbiwebbi@gmail.com <bebbiwebbi@gmail.com@fcdf12c8-19f8-012b-3f5a-fcc496d25d5c>
2012-04-13 01:58:59 +0000

    added dataaccess layer, added bouncy row in rsvp

--- trunk/September22/September22.Web/Site/Rsvp.aspx
+++ trunk/September22/September22.Web/Site/Rsvp.aspx
@@ -1,9 +1,33 @@
-﻿<%@ Page Title="Rsvp" Language="C#" MasterPageFile="~/Master/Site_1Col.master" AutoEventWireup="true" CodeBehind="Rsvp.aspx.cs" Inherits="September22.Rsvp" %>
+﻿<%@ Page Title="Rsvp" Language="C#" MasterPageFile="~/Master/Site_1Col.master" AutoEventWireup="true"
+    CodeBehind="Rsvp.aspx.cs" Inherits="September22.Rsvp" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <script language="javascript" type="text/javascript">
+        function bounce() {
+            $(".bounce").effect("bounce", { times: 5 }, 300);
+        };
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MiddleColumnContent" runat="server">
     <h2><span>RSVP</span></h2>
-    <p>
-        Coming soon!
-    </p>
+    <p>Coming soon!</p>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <div style="margin: 30;">
+                name: <asp:TextBox ID="txtName" runat="server" />
+            </div>
+            <asp:Button runat="server" ID="btnNewGuest" Text="abcd" UseSubmitBehavior="False" 
+                onclick="btnNewGuest_Click" />
+            <br />
+            <asp:ListView runat="server" ID="lvGuests" ondatabound="lvGuests_DataBound">
+                <ItemTemplate>
+                    <asp:Panel runat="server" ID="bounceControl">
+                        <div class="listview rounded-corner">
+                            <asp:Label Text='<%# Eval("Name") %>' runat="server" /><br />
+                        </div>
+                    </asp:Panel>
+                </ItemTemplate>
+            </asp:ListView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
 </asp:Content>
--- trunk/September22/September22.Web/Site/Rsvp.aspx.cs
+++ trunk/September22/September22.Web/Site/Rsvp.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using September22.Common;
+using September22.DataAccess;
 
 namespace September22
 {
@@ -12,7 +13,63 @@ namespace September22
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //WeddingEntities entities = new WeddingEntities();
 
+            //DropDownList1.DataSource = entities.DinnerPreferences;
+            //DropDownList1.DataTextField = "Name";
+            //DropDownList1.DataValueField = "ID";
+            //DropDownList1.DataBind();
+
+            ScriptManager.RegisterStartupScript(UpdatePanel1, UpdatePanel1.GetType(), string.Empty, "bounce();", true);
         }
+
+        protected void btnNewGuest_Click(object sender, EventArgs e)
+        {
+            //check viewstate
+            List<Person> persons;
+            if (ViewState["persons"] != null)
+            {
+                persons = ViewState["persons"] as List<Person>;
+            }
+            else
+            {
+                persons = new List<Person>();
+            }
+
+            //add new person
+            Person newPerson = new Person() { Name = txtName.Text };
+            persons.Add(newPerson);
+            
+            ViewState["persons"] = persons;
+
+            //update listview
+            lvGuests.DataSource = persons;
+            lvGuests.DataBind();
+
+            UpdatePanel1.Update();
+        }
+
+        protected void lvGuests_DataBound(object sender, EventArgs e)
+        {
+            ListView lv = sender as ListView;
+            if (lv != null)
+            {
+                ListViewDataItem lastItem = lv.Items.LastOrDefault();
+                if (lastItem != null)
+                {
+                    WebControl control = lastItem.FindControl("bounceControl") as WebControl;
+                    if (control != null)
+                    {
+                        control.CssClass = "bounce";
+                    }
+                }
+            }
+        }
+    }
+    
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
     }
 }
